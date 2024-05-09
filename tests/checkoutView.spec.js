34ef6907d555351897ed555064faf059d921fc9f
shoghe <shogher.harutyunyan.98@gmail.com>
2024-05-10 03:35:57 +0400

    minor changes

--- tests/checkoutView.spec.js
+++ tests/checkoutView.spec.js
@@ -13,7 +13,60 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Verify that shopping cart button returns "Your cart" page', async ({ page }) => {
+  await page.goto('/inventory.html');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
   await page.goto('/checkout-step-two.html');
   await page.locator('[data-test="shopping-cart-link"]').click();
   await expect(page.locator('[data-test="title"]')).toHaveText('Your Cart');
 });
+
+test('Verify by clicking item title button returns one item page', async ({ page }) => {
+  await page.goto('/inventory.html');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.goto('/checkout-step-two.html');
+  await page.locator('[data-test="inventory-item-name"]').click();
+  await expect(page.getByText('Back to products')).toBeVisible();
+});
+
+test('Verify That the Cancel Button Returns to the Product Page', async ({ page }) => {
+  await page.goto('/checkout-step-two.html');
+  await page.locator('[data-test="cancel"]').click();
+  await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+});
+
+test('Verify that the Finish Button redirects to the Complete Page', async ({ page }) => {
+  await page.goto('/checkout-step-two.html');
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Complete!');
+});
+
+test('Verify That the "tax" is 8% of "Item Total", async', async ({ page }) => {
+  const total = page.locator('[data-test="subtotal-label"]').textContent() * 0.08;
+  await page.goto('/inventory.html');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.goto('/checkout-step-two.html');
+  await expect(page.locator('[data-test="tax-label"]').textContent()).toBe(total);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test('Verify That the "Item Total" is the sum of all chosen item prices', async ({ page }) => {
+//   const value1 = page.getByText('$29.99').textContent();
+//   const value2 = page.getByText('$9.99').textContent();
+//   const total = value1 + value2;
+//   await page.goto('/inventory.html');
+//   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+//   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+//   await page.goto('/checkout-step-two.html');
+//   await expect(page.locator('[data-test="summary-total"]').textContent()).toBe(total);
+// });
