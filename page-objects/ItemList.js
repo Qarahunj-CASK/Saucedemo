48b5a2b4b2263f5519b6178d7aad7c9f579ce4ce
MTuryan <mariam-khachaturyan@mail.ru>
2024-05-16 21:28:20 +0400

    Updated constructr

--- page-objects/ItemList.js
+++ page-objects/ItemList.js
@@ -1,11 +1,24 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/no-extraneous-dependencies */
+
 exports.ItemsList = class ItemsList {
   constructor(page) {
     this.page = page;
-    this.itemname1 = page.locator('.inventory_item_name').nth(0);
-    this.addtocart = page.locator('#add-to-cart-sauce-labs-backpack').nth(0);
+    this.itemname1 = page.locator('.inventory_item_name');
+    this.addtocart = page.locator('.btn.btn_primary.btn_small.btn_inventory');
     this.shoppingcart = page.locator('.shopping_cart_link');
-    this.removecart = page.locator('#remove-sauce-labs-backpack').nth(0);
+    this.removecart = page.locator('#remove-sauce-labs-backpack');
+  }
+
+  async clickonTitle(number) {
+    await this.itemname1.nth(number).click();
+  }
+
+  async clickonShoppingCart() {
+    await this.shoppingcart.click();
+  }
+
+  async addtocartButton(number) {
+    await this.addtocart.nth(number).click();
   }
 };
